af6fff0cbf751ff9faa90563598cb186379b9b7f
npanttonen <niko22020@student.hamk.fi>
2023-10-02 11:14:36 +0300

    nikos combined code

--- New Design/assets/js/chart-temperature.js
+++ New Design/assets/js/chart-temperature.js
@@ -1,5 +1,5 @@
 // Function to fetch data and display it on the webpage
-async function fetchData(roomNumber) {
+async function fetchtemperature(roomNumber, timegap) {
 
     // Define your API key
     const apiKey = 'II6dsQDctGjWeoHgnT5wPjXlyJVmmUbvASnh2Zay';
@@ -22,7 +22,7 @@ async function fetchData(roomNumber) {
         return `${year}-${month}-${day}`;
     }
 
-    // Function to get the date string for one week ago
+    // Function to get the date string for one week, month and year ago
     function getOneWeekAgoDateString() {
         const now = new Date();
         const oneWeekAgo = new Date(now);
@@ -32,13 +32,57 @@ async function fetchData(roomNumber) {
         const day = oneWeekAgo.getDate().toString().padStart(2, '0');
         return `${year}-${month}-${day}`;
     }
+    function getOneMonthAgoDateString() {
+        const now = new Date();
+        const oneMonthAgo = new Date(now);
+        oneMonthAgo.setMonth(oneMonthAgo.getMonth() - 1);   // set the date for one month ago
+        const year = oneMonthAgo.getFullYear();
+        const month = (oneMonthAgo.getMonth() + 1).toString().padStart(2, '0');
+        const day = oneMonthAgo.getDate().toString().padStart(2, '0');
+        return `${year}-${month}-${day}`;
+    }
+    function getOneYearAgoDateString() {
+        const now = new Date();
+        const oneYearAgo = new Date(now);
+        oneYearAgo.setFullYear(oneYearAgo.getFullYear() - 1);   // set the date for one year ago
+        const year = oneYearAgo.getFullYear();
+        const month = (oneYearAgo.getMonth() + 1).toString().padStart(2, '0');
+        const day = oneYearAgo.getDate().toString().padStart(2, '0');
+        return `${year}-${month}-${day}`;
+    }
 
 
     try {
         // Get the current time string and one week ago date string
         const dateString = getCurrentDateString();
         const timeString = getCurrentTimeString();
-        const pastDateString = getOneWeekAgoDateString();
+        var pastDateString = getOneWeekAgoDateString();
+
+        if (timegap == "week"){
+            pastDateString = getOneWeekAgoDateString();
+            Chart.getChart(document.getElementById('chart-temperature')).destroy(); 
+            //color the button after pressed
+            document.getElementById("week").classList.toggle("pressed");
+            document.getElementById("month").classList.remove("pressed");
+            document.getElementById("year").classList.remove("pressed");
+        }
+        else if (timegap == "month"){
+            pastDateString = getOneMonthAgoDateString();
+            Chart.getChart(document.getElementById('chart-temperature')).destroy();
+            document.getElementById("week").classList.remove("pressed");
+            document.getElementById("month").classList.toggle("pressed");
+            document.getElementById("year").classList.remove("pressed");
+        }
+        else if (timegap == "year"){
+            pastDateString = getOneYearAgoDateString();
+            Chart.getChart(document.getElementById('chart-temperature')).destroy();
+            document.getElementById("week").classList.remove("pressed");
+            document.getElementById("month").classList.remove("pressed");
+            document.getElementById("year").classList.toggle("pressed");
+        }
+        else{
+            document.getElementById("week").classList.toggle("pressed"); 
+        }
 
         // Update the apiUrl with the time strings
         const apiUrl = 'https://iot.research.hamk.fi/api/v1/hamk/rooms/tsdata?room-id=' + roomNumber + '&startTime=' + pastDateString + 'T' + timeString + 'Z&endTime=' + dateString + 'T' + timeString + 'Z&fields=temperature';
@@ -88,7 +132,7 @@ async function fetchData(roomNumber) {
                     label: "Temperature",
                     tension: 0,
                     borderWidth: 0,
-                    pointRadius: 5,
+                    pointRadius: 0,
                     pointBackgroundColor: "rgba(255, 255, 255, .8)",
                     pointBorderColor: "transparent",
                     borderColor: "rgba(255, 255, 255, .8)",
